1441dba88b41cae00358d6d816f3c519e444da5d
Gilberto Velez <gvelezjr02@gmail.com>
2019-04-09 10:38:35 -0400

    add saigon thumb as poster to video

--- src/js/app/video.js
+++ src/js/app/video.js
@@ -4,13 +4,15 @@ import ReactDOM from 'react-dom';
 
 import saigon from './../../assets/videos/saigon_720p.mp4';
 
+import thumb from './../../assets/images/saigon_thumb_720p.jpg';
+
 export default class App extends Component {
 
 	render() {
 
 		return <div className="video">
 
-			<video autoPlay preLoad='auto' loop muted='muted' playsInline='playsinline'>
+			<video autoPlay preLoad='auto' loop muted='muted' playsInline='playsinline' poster={thumb}>
 
 				<source src={saigon} />
 
