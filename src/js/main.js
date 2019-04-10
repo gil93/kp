b87c06b93bc94d23ba6a7e4753b7d458f2f3dc66
Gil <gvelezjr02@gmail.com>
2019-04-10 02:34:08 -0400

    update main module

--- src/js/main.js
+++ src/js/main.js
@@ -6,21 +6,119 @@ import Video from './app/video';
 
 import Intro from './app/intro';
 
+import Sidebar from './app/sidebar';
+
+import Footer from './app/footer';
+
+import Imagesloaded from 'imagesloaded';
+
+import anime from 'animejs';
+
 export default class App extends Component {
 
 	constructor( props ) {
 
 		super( props );
 
+		this.state = {
+
+			loaded: false,
+			...props
+
+		};
+
+		this.spinnerRef = React.createRef();
+
+	}
+
+	componentDidMount() {
+
+		const { name } = this.state;
+
+		new Imagesloaded( `.${name}`, () => {
+
+			let $spinner = this.spinnerRef.current;
+
+			anime({
+
+				targets: $spinner.getElementsByClassName( 'dot' ),
+				translateY: -25,
+				direction: 'alternate',
+				easing: 'easeOutElastic(1, .6)',
+				// loop: 4,
+				loop: 0,
+				delay: anime.stagger( 300, { start: 300 } ),
+				complete: () => {
+
+					anime({
+
+						targets: $spinner,
+						opacity: 0,
+						duration: .5,
+						easing: 'linear',
+						complete: () => {
+
+							this.setState( () => ({ loaded: true } ));
+
+						}
+
+					})
+
+				}
+
+			})
+
+		});
+
 	}
 
 	render() {
 
+		const { loaded } = this.state;
+
+		const loadClass = loaded ? 'loaded' : 'loading';
+
 		return <div ref={this.mainRef} className="main">
 
-			<Video />
+			<div className="spinner" ref={this.spinnerRef}>
 
-			<Intro />
+				<ul className="dots">
+
+					<li className="dot-container">
+
+						<div className="dot"></div>
+
+					</li>
+
+					<li className="dot-container">
+
+						<div className="dot"></div>
+
+					</li>
+
+					<li className="dot-container">
+
+						<div className="dot"></div>
+
+					</li>
+
+				</ul>
+
+			</div>
+
+			<div className={loadClass}>
+
+				<Video />
+
+				<a className="logo" href="/">GIL</a>
+
+				<Intro />
+
+				<Sidebar />
+
+				<Footer />
+
+			</div>
 
 		</div>
 
